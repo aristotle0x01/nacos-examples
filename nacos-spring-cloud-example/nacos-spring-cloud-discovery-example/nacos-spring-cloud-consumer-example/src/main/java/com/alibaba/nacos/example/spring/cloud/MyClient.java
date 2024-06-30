e42d2496a09d4add6c52a54fec272b5d04bc1647
aristotle0x01 <aristotle0x01@protonmail.com>
2024-06-30 11:21:46 +0800

    minor

--- nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-consumer-example/src/main/java/com/alibaba/nacos/example/spring/cloud/MyClient.java
+++ nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-consumer-example/src/main/java/com/alibaba/nacos/example/spring/cloud/MyClient.java
@@ -13,6 +13,15 @@ public interface MyClient {
     @RequestMapping(method = RequestMethod.GET, value = "/srv")
     String echo2(@RequestParam("param") String param);
 
+
+//    feign form
+//    https://stackoverflow.com/questions/55624656/java-lang-noclassdeffounderror-feign-requestbody-in-feign-while-adding-support
+//    https://stackoverflow.com/questions/31752779/file-upload-using-feign-multipart-form-data
+//    https://stackoverflow.com/questions/35803093/how-to-post-form-url-encoded-data-with-spring-cloud-feign
+//    https://howtoprogram.xyz/2016/12/29/file-uploading-open-feign/
+//    https://mkyong.com/spring-boot/spring-boot-file-upload-example/
+
+
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Headers("Content-Type: multipart/form-data")
     ResponseEntity<String> uploadFiles(@RequestPart("files") MultipartFile file);
